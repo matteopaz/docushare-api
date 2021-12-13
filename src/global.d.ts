9ef387ada2b1694866d5b068b2b1eb1fac6c5306
matteopaz <86904497+matteopaz@users.noreply.github.com>
2021-12-13 13:09:09 -0800

    Changed domain

--- src/global.d.ts
+++ src/global.d.ts
@@ -24,6 +24,6 @@ declare global {
 
 export const FRONTEND_PROD_ORIGIN = "https://docushare.matteopaz.me"; // Production URL Origin
 export const FRONTEND_STAGING_ORIGIN = "https://docushare-lite.matteodev.workers.dev"; // Staging URL Origin
-export const API_PROD_ORIGIN = "https://api.docushare.matteopaz.me"; // Production URL Origin
+export const API_PROD_ORIGIN = "https://docushare-api.matteopaz.me"; // Production URL Origin
 export const API_STAGING_ORIGIN = "https://docushare-api-staging.matteodev.workers.dev"; // Staging URL Origin
 
